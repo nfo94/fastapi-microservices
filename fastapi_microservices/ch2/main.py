33faf116bc62cd7d00284ad7d5a50b6c90913924
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2025-01-20 12:58:31 +0000

    continue study

--- fastapi_microservices/ch2/main.py
+++ fastapi_microservices/ch2/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
-from .routes.login import user
-from .routes.places import destination
+from login import user
+from manager import manager
+from places import destination
+from visitor import visitor
+from feedback import post
 
 app = FastAPI()
 
+app.include_router(manager.router)
 app.include_router(user.router)
 app.include_router(destination.router)
+app.include_router(visitor.router)
+app.include_router(post.router, prefix="/post")
